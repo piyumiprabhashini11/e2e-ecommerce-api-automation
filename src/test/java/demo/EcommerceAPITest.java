adb0fe45b288e0f3db1cbf140f9e07d2b5055fee
piyumiprabhashini11 <piyumimadduamge1700@gmail.com>
2026-01-05 09:22:40 +0000

    Create/Add new product

--- src/test/java/demo/EcommerceAPITest.java
+++ src/test/java/demo/EcommerceAPITest.java
@@ -3,12 +3,15 @@ package demo;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import pojo.LoginRequest;
 import pojo.LoginResponse;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
+
+import java.io.File;
 
 public class EcommerceAPITest {
 
@@ -26,5 +29,26 @@ public class EcommerceAPITest {
 			System.out.println(loginResponse.getUserId());
 			System.out.println(loginResponse.getToken());
 			System.out.println(loginResponse.getMessage());
+			
+			//Add Product
+			String token=loginResponse.getToken();
+			String UserId=loginResponse.getUserId();
+			RequestSpecification req2=new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com/").addHeader("authorization", token).build();
+			
+			RequestSpecification reqCreateProduct=given().spec(req2).param("productName", "Lewis")	.param("productAddedBy",UserId).param("productCategory","fashion")
+					.param("productSubCategory","shirt").param("productPrice",11500).param("productDescription","Lewis Originals").param("productFor","women")
+					.multiPart("productImage",new File("E:\\Piyumi\\Automation\\Rest Assured API Automation\\src\\test\\resources\\frock.png"));
+			
+			//Method A - Get the response into a String variable and create the JsonPath object (Json object) by inserting that String variable
+			String responseCreateProduct=reqCreateProduct.when().post("/api/ecom/product/add-product").then().extract().response().asString(); 
+			JsonPath js=new JsonPath(responseCreateProduct);
+			System.out.println(js.getString("productId"));
+			System.out.println(js.getString("message"));
+			
+			//Method B Get the response as a JsonPath object (Json object) directly
+			JsonPath resCreateProduct=reqCreateProduct.when().post("/api/ecom/product/add-product").then().extract().response().jsonPath();
+			System.out.println(resCreateProduct.getString("productId"));
+			System.out.println(resCreateProduct.getString("message"));	
+			
 		}
 }
